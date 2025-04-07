571949770d8bdb4390684cca86fb26fdbd235a81
Revekes <rosiak.patryk27@gmail.com>
2025-04-07 12:53:27 +0200

    Create Dokumentacja_3.3.py

---
+++ Zadanie_1/Dokumentacja_3.3.py
@@ -0,0 +1,17 @@
+# Zadanie 1 - Kurs Wstęp do Pythona
+# Patryk Rosiak
+# Repozytorium: https://github.com/Revekes/python-intro
+
+
+# 1. Opis funkcja wbudowana: zip()
+# Łączy elementy z dwóch lub więcej iterowalnych obiektów w pary.
+# https://docs.python.org/3/library/functions.html#zip
+
+# 2. OPis moduł standardowy: random
+# Umożliwia generowanie losowych liczb.
+# https://docs.python.org/3/library/random.html
+
+# 3. Opis wyjątek: ValueError
+# Pojawia się, gdy operacja i/lub funkcja otrzymują nieprawidłową wartość. 
+# https://docs.python.org/3/library/exceptions.html#ValueError
+
